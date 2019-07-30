27c315914a089b7044bf82f85b977f0467756020
Lain_OS <53471160+Lain-OS@users.noreply.github.com>
2019-07-30 10:24:28 +0000

    .emacs.el

---
+++ .emacs.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(elfeed-feeds
+   (quote
+    ( ADD YOUR OWN FEEDS HERE)))
+ '(quote
+   (org-export-backends
+    (quote
+     (Ascii beamer html icalendar latex md odt groff rss)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "goldenrod" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
+
+
+
+;;
+(org-babel-load-file (expand-file-name "~/.cof.org"))
+
